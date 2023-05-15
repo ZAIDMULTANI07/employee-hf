d711bb1572dc39bde531e5055e3994d3a2f96848
Mohd Zaid Multani <mohdzaidmultani@Mohds-MacBook-Air.local>
2023-05-15 22:18:53 +0530

    add: employee registeration form added

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ function toggleNav() {
     navMenu.classList.toggle("hidden");
   }
   
+  const employeeForm = document.getElementById('employee-form');
+
+employeeForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const position = document.getElementById('position').value;
+  const about = document.getElementById('about').value;
+  const joiningDate = document.getElementById('joining-date').value;
+
+  if (!name || !position || !about || !joiningDate) {
+    alert('Please fill in all the required fields.');
+    return;
+  }
+
+  const employeeData = {
+    name,
+    position,
+    about,
+    joiningDate,
+  };
+
+  let employees = [];
+
+  if (localStorage.getItem('employees')) {
+    employees = JSON.parse(localStorage.getItem('employees'));
+  }
+
+  employees.push(employeeData);
+
+  localStorage.setItem('employees', JSON.stringify(employees));
+
+  window.location.href = '/employee-listing.html';
+});
