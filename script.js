752da149b5d5ae96f417b03ce3c4b58a0ed1db29
Mohd Zaid Multani <mohdzaidmultani@Mohds-MacBook-Air.local>
2023-05-16 10:19:58 +0530

    add: employee listing page

--- script.js
+++ script.js
@@ -39,87 +39,25 @@ employeeForm.addEventListener('submit', (event) => {
   window.location.href = '/employee-listing.html';
 });
 
+//EMPLOYEE-LISTING
+// get the table element
+const table = document.getElementById('employee-table');
 
-// Function to toggle the navigation menu for small screens
-function toggleNav() {
-    var navMenu = document.getElementById("nav-menu");
-    if (navMenu.style.display === "block") {
-        navMenu.style.display = "none";
-    } else {
-        navMenu.style.display = "block";
-    }
-}
+// attach event listener to the table
+table.addEventListener('click', function(event) {
+  // check if the clicked element is a delete button
+  if (event.target.classList.contains('delete-btn')) {
+    // get the row element to be deleted
+    const row = event.target.closest('tr');
+    // remove the row from the table
+    row.remove();
+    // get the id of the employee to be deleted
+    const id = row.dataset.id;
+    // remove the employee from the browser storage
+    const employees = JSON.parse(localStorage.getItem('employees'));
+    const updatedEmployees = employees.filter(employee => employee.id !== id);
+    localStorage.setItem('employees', JSON.stringify(updatedEmployees));
+  }
+});
 
-// Function to delete an employee from the table and localStorage
-function deleteEmployee(index) {
-    if (confirm("Are you sure you want to delete this employee?")) {
-        // Retrieve the employees array from localStorage
-        var employees = JSON.parse(localStorage.getItem("employees"));
 
-        // Remove the employee at the given index
-        employees.splice(index, 1);
-
-        // Update the employees array in localStorage
-        localStorage.setItem("employees", JSON.stringify(employees));
-
-        // Reload the page to update the table
-        location.reload();
-    }
-}
-
-// Function to display the employees in the table
-function displayEmployees() {
-    // Retrieve the employees array from localStorage
-    var employees = JSON.parse(localStorage.getItem("employees"));
-
-    // Get the table body element
-    var tableBody = document.getElementById("employee-table-body");
-
-    // Loop through the employees and add them to the table
-    for (var i = 0; i < employees.length; i++) {
-        var employee = employees[i];
-
-        // Create a new row element
-        var row = document.createElement("tr");
-
-        // Add the employee name to the row
-        var nameCell = document.createElement("td");
-        nameCell.textContent = employee.name;
-        row.appendChild(nameCell);
-
-        // Add the employee position to the row
-        var positionCell = document.createElement("td");
-        positionCell.textContent = employee.position;
-        row.appendChild(positionCell);
-
-        // Add the employee about to the row
-        var aboutCell = document.createElement("td");
-        aboutCell.textContent = employee.about;
-        row.appendChild(aboutCell);
-
-        // Add the employee joining date to the row
-        var joiningDateCell = document.createElement("td");
-        joiningDateCell.textContent = employee.joiningDate;
-        row.appendChild(joiningDateCell);
-
-        // Add the delete button to the row
-        var deleteButtonCell = document.createElement("td");
-        var deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.onclick = (function(index) {
-            return function() {
-                deleteEmployee(index);
-            }
-        })(i);
-        deleteButtonCell.appendChild(deleteButton);
-        row.appendChild(deleteButtonCell);
-
-        // Add the row to the table body
-        tableBody.appendChild(row);
-    }
-}
-
-// Call the displayEmployees function on page load
-window.onload = function() {
-    displayEmployees();
-};
